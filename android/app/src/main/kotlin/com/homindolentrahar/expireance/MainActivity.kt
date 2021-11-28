c5ce37fbdbcc7f7227d2529c972049644629b5e7
Rahardiyan E. Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2021-11-28 12:42:00 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/homindolentrahar/expireance/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.homindolentrahar.expireance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Expireance',
+      home: Root(),
+    );
+  }
+}
+
+class Root extends StatelessWidget {
+  const Root({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text("Hello there 👋🏼"),
+        ),
+      ),
+    );
+  }
+}
